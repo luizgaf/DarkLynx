8e8a7f9d29af783e66190ece28bb9a5852d0d580
luizgaf <luiz.guilhermeaf21@gmail.com>
2025-03-16 22:10:21 -0300

    first commit

---
+++ darklynx.py
@@ -0,0 +1,35 @@
+import socket
+import argparse
+import time # estm.time
+
+ART = r"""
+    ___           _      __                  
+   /   \__ _ _ __| | __ / / _   _ _ __ __  __
+  / /\ / _` | '__| |/ // / | | | | '_ \\ \/ /
+ / /_// (_| | |  |   </ /__| |_| | | | |>  < 
+/___,' \__,_|_|  |_|\_\____/\__, |_| |_/_/\_\
+                            |___/            
+"""
+NAME = "DarkLynx Network Reconnaissance & Port Scanning Suite"
+SLOG = "    Unseen. Unstoppable. Uncover every port."
+
+def scan_port(host, port):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(1)
+        r = sock.connect_ex((host,port))
+        if r == 0:
+            print(f"Port {port} in {host}: OPEN")
+        else:
+            print(f"Port {port} in {host}: CLOSED")
+        sock.close
+    except socket.error:
+        print(f"Unable to connect port.")
+
+def main():
+    print("\n \n \n" + ART + "\n" + NAME + "\n" + SLOG + "\n \n \n")
+
+
+
+if __name__ == "__main__":
+    main()
